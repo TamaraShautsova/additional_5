c80687dbfa1eb7a4f5d2c86ba679a10a302ab451
tamara shautsova <t.shautsova@gmail.com>
2018-09-23 16:48:45 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var matches = {};
+    var sequence  = [];
+    for (var i = 0; i < bracketsConfig.length; i++) {
+        matches[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
+    for (var i = 0; i < str.length; i++) {
+        if (matches[str[i]]) {
+            if (matches[str[i]] == str[i] && sequence .indexOf(str[i]) != -1) {
+                var element = sequence .pop();
+                if (element != str[i]) {
+                    return false;
+                }
+            } else {
+              sequence .push(str[i]);
+            }
+        } else {
+            if (str[i] != matches[sequence .pop()]) {
+                return false;
+            }
+        }
+    }
+    return sequence .length === 0;
 }
